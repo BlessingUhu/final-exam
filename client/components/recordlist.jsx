deb61626792a4455860e87ef02b321983caf2948
BlessingUhu <Blessing2002.feb@gmail.com>
2023-12-18 00:56:34 -0500

    trying to remove node_modules

---
+++ client/components/recordlist.jsx
@@ -0,0 +1,28 @@
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+
+const RecordsList = () => {
+  const [records, setRecords] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("/record")
+      .then((response) => setRecords(response.data))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
+  return (
+    <div>
+      <h2>Records List</h2>
+      <ul>
+        {records.map((record) => (
+          <li key={record._id}>
+            {record.name}, {record.position}, {record.level}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default RecordsList;
